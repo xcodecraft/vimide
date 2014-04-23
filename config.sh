f8df70108258bfcba468626fa81024dbaf670546
leiming <leiming@xen03.wd.corp.qihoo.net>
2014-04-23 16:34:39 +0800

    Fix the bug on config's path

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@
 # Vim备份目录
 export BACKUP_DIR=$HOME/backup/vim_config_bk
 # 旧版vimrc文件 
-export OLD_VIMRC=/home/q/tools/game_team/vim/runtime/_vimrc_local 
+export OLD_VIMRC=/home/q/tools/game_team/vim/runtime/_vimrc 
 # 旧版.vim文件夹位置
 export OLD_VIMDIR=/home/q/tools/game_team/vim/runtime 
 # vim_ide文件夹位置
