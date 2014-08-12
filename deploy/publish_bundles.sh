5dfd55db74a19b38fbf093a6cd22f232b46fc57f
leiming <leming@360.cn>
2014-08-12 18:44:50 +0800

    finish deplory

--- deploy/publish_bundles.sh
+++ deploy/publish_bundles.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
 BASEDIR=$(cd $(dirname $BASH_SOURCE);pwd)
-
-USER=leiming
-
 source $BASEDIR/../config.sh
+if [[ -z "$1" ]]; then
+   SOURCE_DIR=$CURRENT_VIMIDE
+elif [[ ! -d "$1" ]]; then
+    echo "路径无效或不可读"
+    return 0
+else
+    SOURCE_DIR=$1
+fi
+
+if [[ -z "$2" ]]; then
+    USER=$(whoami)
+else
+    USER=$2
+fi
+
+TARGET_IP="10.16.73.21"
+
 while read <&10 remote_ip; do
-    ssh $remote_ip "echo $TEAM_BUNDLE_DIR; test -d $TEAM_BUNDLE_DIR && echo true ||  echo false"
-    # ssh $remote_ip "test -d $TEAM_BUNDLE_DIR || sudo mkdir $TEAM_BUNDLE_DIR; sudo chmod 777 $TEAM_BUNDLE_DIR;"
-    rsync -avzh --chmod=ugo=rwx $TEAM_BUNDLE_DIR/* $remote_ip:$TEAM_BUNDLE_DIR
+    ssh -t $remote_ip "if [ ! -d $TEAM_BUNDLE_DIR ]; then sudo mkdir -p $TEAM_BUNDLE_DIR; fi; sudo chmod 777 -R $TEAM_BUNDLE_DIR; sudo rsync -avz --delete --force $USER@$TARGET_IP:$TEAM_BUNDLE_DIR/ $TEAM_BUNDLE_DIR/ ;"
+    # ssh -t $remote_ip "test -d $TEAM_BUNDLE_DIR || sudo mkdir $TEAM_BUNDLE_DIR;"
+    # ssh -t $remote_ip "test -r $TEAM_BUNDLE_DIR || sudo chmod 777 $TEAM_BUNDLE_DIR -R;"
+    # rsync -avzh --delete --chmod=ugo=rwx $TEAM_BUNDLE_DIR/* $remote_ip:$TEAM_BUNDLE_DIR
     echo -e "---- 同步 $remote_ip:$TEAM_BUNDLE_DIR 完成----\n"
 done 10< testiplist
 
--- deploy/publish_vimide.sh
+++ deploy/publish_vimide.sh
@@ -4,16 +4,24 @@ BASEDIR=$(cd $(dirname $BASH_SOURCE);pwd)
 source $BASEDIR/../config.sh
 if [[ -z "$1" ]]; then
    SOURCE_DIR=$CURRENT_VIMIDE
-elif [[ ! -rd "$1" ]]; then
+elif [[ ! -d "$1" ]]; then
     echo "路径无效或不可读"
     return 0
 else
     SOURCE_DIR=$1
 fi
 
+if [[ -z "$2" ]]; then
+    USER=$(whoami)
+else
+    USER=$2
+fi
+
+TARGET_IP="10.16.73.21"
+
 while read <&10 remote_ip; do
-    ssh $remote_ip "[[ ! -d $SOURCE_DIR ]] && sudo mkdir $SOURCE_DIR; sudo chmod 777 $SOURCE_DIR; || ([[ ! -r $SOURCE_DIR ]] && sudo chmod 777 $SOURCE_DIR;)"
-    rsync -avzh --chmod=ugo=rwx $SOURCE_DIR $remote_ip:$SOURCE_DIR
+    ssh -t $remote_ip "if [ ! -d $SOURCE_DIR ]; then sudo mkdir -p $SOURCE_DIR; fi; sudo chmod 777 -R $SOURCE_DIR;sudo rsync -avz --delete --force $USER@$TARGET_IP:$SOURCE_DIR/ $SOURCE_DIR/ ;"
+    # sudo rsync -avz --delete --progress --force $SOURCE_DIR/ leiming@$remote_ip:$SOURCE_DIR/
     echo -e "---- 同步 $remote_ip:$SOURCE_DIR 完成----\n"
 done 10< testiplist
 
