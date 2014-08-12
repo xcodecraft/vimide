24191a044f4a3f000b2d067f80450b1e760bc06d
leiming <leming@360.cn>
2014-08-12 14:18:54 +0800

    add cscope and ctags

--- deploy/publish_bundles.sh
+++ deploy/publish_bundles.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 BASEDIR=$(cd $(dirname $BASH_SOURCE);pwd)
+
+USER=leiming
+
 source $BASEDIR/../config.sh
 while read <&10 remote_ip; do
-    ssh $remote_ip "[[ ! -rd $TEAM_BUNDLE_DIR ]] && sudo mkdir $TEAM_BUNDLE_DIR; sudo chmod 777 $TEAM_BUNDLE_DIR;"
+    ssh $remote_ip "echo $TEAM_BUNDLE_DIR; test -d $TEAM_BUNDLE_DIR && echo true ||  echo false"
+    # ssh $remote_ip "test -d $TEAM_BUNDLE_DIR || sudo mkdir $TEAM_BUNDLE_DIR; sudo chmod 777 $TEAM_BUNDLE_DIR;"
     rsync -avzh --chmod=ugo=rwx $TEAM_BUNDLE_DIR/* $remote_ip:$TEAM_BUNDLE_DIR
     echo -e "---- 同步 $remote_ip:$TEAM_BUNDLE_DIR 完成----\n"
-done 10< iplist
+done 10< testiplist
 
