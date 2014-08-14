66dd2e52357db05fa34382c0baeb53789c41400d
leiming <leming@360.cn>
2014-08-14 17:46:05 +0800

    fix cscope bug

--- deploy/publish_bundles.sh
+++ deploy/publish_bundles.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
+if [[ -r "$1" ]]; then
+    IPFILE=$1
+else
+    IPFILE='testiplist'
+fi
+
 BASEDIR=$(cd $(dirname $BASH_SOURCE);pwd)
 source $BASEDIR/../config.sh
-if [[ -z "$1" ]]; then
+if [[ -z "$2" ]]; then
    SOURCE_DIR=$CURRENT_VIMIDE
-elif [[ ! -d "$1" ]]; then
+elif [[ ! -d "$2" ]]; then
     echo "路径无效或不可读"
     return 0
 else
-    SOURCE_DIR=$1
+    SOURCE_DIR=$2
 fi
 
-if [[ -z "$2" ]]; then
+if [[ -z "$3" ]]; then
     USER=$(whoami)
 else
-    USER=$2
+    USER=$3
 fi
 
 TARGET_IP="10.16.73.21"
@@ -25,5 +31,5 @@ while read <&10 remote_ip; do
     # ssh -t $remote_ip "test -r $TEAM_BUNDLE_DIR || sudo chmod 777 $TEAM_BUNDLE_DIR -R;"
     # rsync -avzh --delete --chmod=ugo=rwx $TEAM_BUNDLE_DIR/* $remote_ip:$TEAM_BUNDLE_DIR
     echo -e "---- 同步 $remote_ip:$TEAM_BUNDLE_DIR 完成----\n"
-done 10< testiplist
+done 10< $IPFILE
 
