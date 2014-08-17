bff172d863b1ac81871a459610ccae10c7362eea
leiming <leming@360.cn>
2014-08-17 15:06:49 +0800

    fix sed bug

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 # BASEDIR=$(dirname $0)
 BASEDIR=$(cd $(dirname $BASH_SOURCE);pwd)
-source $BASEDIR/config.sh 
+source $BASEDIR/config.sh
 
 if [[ "$1" = "-h" ]]; then
     echo ""
@@ -14,7 +14,7 @@ if [[ "$1" = "-h" ]]; then
 fi
 
 echo -e "---- 备份VJ ----\n"
-bash $BASEDIR/backup.sh 
+bash $BASEDIR/backup.sh
 
 echo -e "---- 更新.vimrc ----\n"
 if [[ "$1" = "-f" ]]; then
@@ -24,17 +24,17 @@ else
 fi
 
 echo -e "---- 更新.vim ----\n"
-bash $BASEDIR/catvimdir.sh 
+bash $BASEDIR/catvimdir.sh
 
 echo -e "---- 更新.bash_profile ----\n"
 sed -i -e '/_vj_bashrc/d' ~/.bash_profile
-sed -i -e '/alias\ vj/d' ~/.bash_profile
+sed -i -e '/alias\s*vj/d' ~/.bash_profile
 
-echo -e "alias  vjbackup=\"bash $BASEDIR/backup.sh\""      >> ~/.bash_profile
-echo -e "alias  vjrecovery=\"bash $BASEDIR/recovery.sh\""  >> ~/.bash_profile
-echo -e "alias  vjupdate=\"bash $BASEDIR/update.sh\""      >> ~/.bash_profile
-echo -e "alias  vj=\"vim '+call VjOpen()'\""               >> ~/.bash_profile
-echo -e "source $BASEDIR/_vj_bashrc"                       >> ~/.bash_profile
+echo -e "alias vjbackup=\"bash $BASEDIR/backup.sh\""      >> ~/.bash_profile
+echo -e "alias vjrecovery=\"bash $BASEDIR/recovery.sh\""  >> ~/.bash_profile
+echo -e "alias vjupdate=\"bash $BASEDIR/update.sh\""      >> ~/.bash_profile
+echo -e "alias vj=\"vim '+call VjOpen()'\""               >> ~/.bash_profile
+echo -e "source $BASEDIR/_vj_bashrc"                      >> ~/.bash_profile
 
 read -p "VJ已经更新完成，请问需要立即生效吗？（否则将在下次登录时生效）[ctrl+c]取消(y/n) y:" isSource
 case $isSource in
